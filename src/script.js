f3322e7c16c469312a36e98374e7f13a835dbfd0
[noe andreu serrano] <[nandreuse@uoc.edu]>
2026-04-02 20:48:46 +0200

    Actualización web

--- src/script.js
+++ src/script.js
@@ -1,26 +1,21 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
+import 'aos/dist/aos.css';
+
+import AOS from 'aos';
+
+AOS.init({
+  duration: 1000, 
+  once: true,     
+});
 
 const swiper = new Swiper('.swiper', {
   modules: [Navigation, Pagination, Autoplay],
-  /*loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },*/
-  loop: true,              // bucle infinito
-  slidesPerView: 1,        // solo 1 slide visible
-  spaceBetween: 0,          // espacio entre slides
-  autoplay: {               // autoplay activo
+  loop: true,              
+  slidesPerView: 1,        
+  spaceBetween: 0,          
+  autoplay: {               
     delay: 3000,
     disableOnInteraction: false,
   },
